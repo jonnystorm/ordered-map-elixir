207d0f3aaba5e29b4ff14b23c28ce163282ea623
Jonathan Storm <the.jonathan.storm@gmail.com>
2016-09-21 15:06:12 -0500

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -2,12 +2,16 @@ defmodule OrderedMap.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :ordered_map,
-     version: "0.0.1",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [ app: :ordered_map,
+      version: "0.0.1",
+      name: OrderedMap,
+      source_url: "https://github.com/jonnystorm/ordered-map-elixir",
+      elixir: "~> 1.3",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+      docs: [extras: ["README.md"]]
+    ]
   end
 
   def application do
