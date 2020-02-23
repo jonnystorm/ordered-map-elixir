11642aaf20b371c8cede8db835ab002838024c16
Jonathan D. Storm <jds@idio.link>
2020-02-23 15:56:23 -0600

    Fix OrderedMap impl of Enumerable protocol in Elixir 1.10
    * Elixir 1.10 changes arity of `Enumerable.List.slice/3`
    * To fix, add `OrderedMap.Utility.slice_list_fun/2` macro
    * New macro selects `slice/3` or `slice/4` in List impl
    * Add test to exercise slice impl
    * Migrate from Travis CI to GitLab CI; auto-deploy docs
    * Update `dev_test.sh`
    * Update deps

---
+++ lib/ordered_map/utility.ex
@@ -0,0 +1,40 @@
+defmodule OrderedMap.Utility do
+  defmacro slice_list_fun(list, size) do
+    fun =
+      fn args ->
+        {:&, [],
+          [
+            { {:., [],
+                [ {:__aliases__,
+                    [alias: false],
+                    [:Enumerable, :List]
+                  },
+                  :slice,
+                ]
+              },
+              [],
+              args
+            }
+          ]
+        }
+      end
+
+    cond do
+      Kernel.function_exported?(
+        Enumerable.List,
+        :slice,
+        3
+      ) ->
+        #&Enumerable.List.slice(unquote(list), &1, &2)
+        fun.([list, {:&, [], [1]}, {:&, [], [2]}])
+
+      Kernel.function_exported?(
+        Enumerable.List,
+        :slice,
+        4
+      ) ->
+        #&Enumerable.List.slice(unquote(list), &1, &2, unquote(size))
+        fun.([list, {:&, [], [1]}, {:&, [], [2]}, size])
+    end
+  end
+end
