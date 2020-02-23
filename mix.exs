8c2e27741ef8fd50bb2265c25916af158055e0f2
James Van Lommel <jamesvl@gmail.com>
2020-02-23 13:52:58 -0500

    Update mix.exs to require Elixir 1.10 or higher

--- mix.exs
+++ mix.exs
@@ -3,10 +3,10 @@ defmodule OrderedMap.Mixfile do
 
   def project do
     [ app: :ordered_map,
-      version: "0.0.4",
+      version: "0.0.5",
       name: "OrderedMap",
       source_url: "https://github.com/jonnystorm/ordered-map-elixir",
-      elixir: "~> 1.3",
+      elixir: "~> 1.10",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps(),
@@ -33,6 +33,6 @@ defmodule OrderedMap.Mixfile do
   end
 
   defp deps do
-    [{:ex_doc, "~> 0.13", only: :dev}]
+    [{:ex_doc, "~> 0.21", only: :dev}]
   end
 end
